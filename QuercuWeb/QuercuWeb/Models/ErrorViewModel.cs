6362f179a8a16d8866e5ecc74aa5b65f9fbf494f
Juan Pablo Martinez <jpms19992015@gmail.com>
2024-01-26 15:36:39 -0600

    Prueba de Repo

---
+++ QuercuWeb/QuercuWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuercuWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
