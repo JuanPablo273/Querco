50f9b3e29a3092f8494cac25e46606a61420c77a
Juan Pablo Martinez <jpms19992015@gmail.com>
2024-01-28 08:37:08 -0600

    Solucion de git

---
+++ QuercuApi/QuercuApi/Entities/OwnerEnt.cs
@@ -0,0 +1,21 @@
+﻿namespace QuercuApi.Entities
+{
+    public class OwnerEnt
+    {
+        public long Id{ get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Telephone { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string IdentificationNumber { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        public bool Status { get; set; }
+
+
+    }
+}
---
+++ QuercuApi/QuercuApi/Entities/PropertyEnt.cs
@@ -0,0 +1,21 @@
+﻿namespace QuercuApi.Entities
+{
+    public class PropertyEnt
+    {
+        public long Id{ get; set; }
+
+        public string PropertyTypeId { get; set;} = string.Empty;
+
+        public string OwnerId { get; set; } = string.Empty;
+
+        public string Number { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        public decimal Area { get; set; } 
+
+        public decimal ConstructionArea { get; set; } 
+
+        public bool Status { get; set; }
+    }
+}
